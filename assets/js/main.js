3fda8e40cea368773b7c111c872fd627f803a506
Tri Che <triche@kms.com.vn>
2020-11-18 16:32:37 +0700

    Update 18/11/20

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,20 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
+	/*======= Skillset *=======*/
 
+	$(".level-bar-inner").css("width", "0");
 
-    /*======= Skillset *=======*/
+	$(window).on("load", function () {
+		const isPrinting = window.location.search === "?print";
 
+		$(".level-bar-inner").each(function () {
+			var itemWidth = $(this).data("level");
 
-    $('.level-bar-inner').css('width', '0');
-
-    $(window).on('load', function() {
-
-        const isPrinting = window.location.search === "?print";
-
-        $('.level-bar-inner').each(function() {
-
-            var itemWidth = $(this).data('level');
-
-            $(this).animate({
-                width: itemWidth
-            }, isPrinting ? 0 : 1000);
-
-        });
-
-    });
-
-
-
+			$(this).animate(
+				{
+					width: itemWidth,
+				},
+				isPrinting ? 0 : 1000
+			);
+		});
+	});
 });
